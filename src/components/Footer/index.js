a2197d1e2c86a489815722922d5df867d9509104
devgui <guilherme.santosfructuoso@gmail.com>
2022-04-11 14:05:11 -0300

    fifth

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,6 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 function Footer() {
   return (
     <div className="container-footer">
-        
       <div className="title-icon">
         <h5>Estúdio D'Mata</h5>
         <AiOutlineLine
@@ -16,21 +15,49 @@ function Footer() {
           size={40}
           margin-left="14px"
         />
-      
-      
-        <div className="link-footer">
-          <AnchorLink href="#sobrenos">Sobre Nós</AnchorLink>
-          <AnchorLink href="#fotos">Fotos</AnchorLink>
-          <AnchorLink href="#servicos">Serviços</AnchorLink>
-          <AnchorLink href="#contato">Contato</AnchorLink>
+
+        <AnchorLink className="footer-link" href="#sobrenos">
+          Sobre Nós
+        </AnchorLink>
+        <AnchorLink className="footer-link" href="#fotos">
+          Fotos
+        </AnchorLink>
+        <AnchorLink className="footer-link" href="#servicos">
+          Serviços
+        </AnchorLink>
+        <AnchorLink className="footer-link" href="#contato">
+          Contato
+        </AnchorLink>
+
+        <img
+          id="img-footer"
+          src={logo}
+          alt="Sistema Logo"
+          width="100px"
+          height="40px"
+        />
+      </div>
+
+      <div className="social">
+        <h5>Social</h5>
+        <AiOutlineLine
+          className="line"
+          color="#24c18c"
+          size={40}
+          margin-left="14px"
+        />
+      </div>
+
+      <div className="parceiros">
+         <h5>Parceiros</h5>
+         <AiOutlineLine
+          className="line"
+          color="#24c18c"
+          size={40}
+          
+        />
         </div>
-       
       </div>
-      <div className="logo-footer">
-        <img src={logo} alt="Sistema Logo" width="100px" height="40px" />
-      </div>
-         
-    </div>
   );
 }
 
